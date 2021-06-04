311df19d9d876b5b574750c5e729e6135b211421
Ranur-react <rahmatnur845@gmail.com>
2021-06-05 03:17:43 +0700

    Upload new branch gn_public_V2

--- application/views/footv2.php
+++ application/views/footv2.php
@@ -105,6 +105,7 @@
 
 		<div class="t-center p-l-15 p-r-15">
 			<div class="t-center p-t-20">
+				xxxx
 				Copyright © XX <?=date('Y');?> <?=ucwords(strtolower($set->nama))?>
 				<?php if($this->func->demo() == true){ ?> | made with <i class="fas fa-heart text-danger"></i> by Masbil</a><?php } ?>
 			</div>
