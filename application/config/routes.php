f68bd01b92079e74da200aa56d351a2ae31750f7
Ranur-react <rahmatnur845@gmail.com>
2021-06-23 20:54:43 +0700

    relounch again

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'home';
-$route['default_controller'] = 'home/mintananceStatus';
+// $route['default_controller'] = 'home/mintananceStatus';
 
 $route['404_override'] = 'home/_404';
 $route['translate_uri_dashes'] = TRUE;
