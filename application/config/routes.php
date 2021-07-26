69916e39a039c7ee389ae3026270273c8f9c217c
Ranur-react <rahmatnur845@gmail.com>
2021-07-26 14:53:42 +0700

    Online kan aja lah lagi daripada tak mengapa

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-// $route['default_controller'] = 'home';
-$route['default_controller'] = 'home/mintananceStatus';
+$route['default_controller'] = 'home';
+// $route['default_controller'] = 'home/mintananceStatus';
 
 $route['404_override'] = 'home/_404';
 $route['translate_uri_dashes'] = TRUE;
